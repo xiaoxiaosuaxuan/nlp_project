7575966f5a2bbc18066cea0529583348ea3bcf19
xiaoxiaosuaxuan <xuyuhangtmx@sjtu.edu.cn>
2022-12-31 00:24:05 +0800

    nothing

--- utils/example.py
+++ utils/example.py
@@ -14,20 +14,23 @@ class Example():
         cls.label_vocab = LabelVocab(root)              #tag('B/I-act-slot') 对应的 id
 
     @classmethod
-    def load_dataset(cls, data_path):         # examples 是 ex 的数组，每个 ex 都包含了一句话里 act-slot-value 以及对应 tag 的信息
+    def load_dataset(cls, data_path, test=False):         # examples 是 ex 的数组，每个 ex 都包含了一句话里 act-slot-value 以及对应 tag 的信息
         datas = json.load(open(data_path, 'r', encoding='utf-8'))
         examples = []
         for data in datas:
             for utt in data:
-                ex = cls(utt)
+                ex = cls(utt, test)
                 examples.append(ex)
         return examples
 
-    def __init__(self, ex: dict):
+    def __init__(self, ex: dict, test):
         super(Example, self).__init__()
         self.ex = ex
 
         self.utt = ex['asr_1best']                #utt是语音转换成的语句
+        self.input_idx = [Example.word_vocab[c] for c in self.utt]                #self.input_idx 即将语句里的字转换成id的列表
+        if test:
+            return
         self.slot = {}                             # self.slot 是 这句话里 {act-slot : value} 的字典，value 就是语句里的词
         for label in ex['semantic']:             #ex['semantic']是 这句话里 act-slot-value 的列表
             act_slot = f'{label[0]}-{label[1]}'
@@ -41,6 +44,5 @@ class Example():
                 self.tags[bidx: bidx + len(value)] = [f'I-{slot}'] * len(value)      
                 self.tags[bidx] = f'B-{slot}'
         self.slotvalue = [f'{slot}-{value}' for slot, value in self.slot.items()]    #self.slotvalue 是 'act-slot-value' 的列表
-        self.input_idx = [Example.word_vocab[c] for c in self.utt]                #self.input_idx 即将语句里的字转换成id的列表
         l = Example.label_vocab
         self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]           # 将tags里的标签转换成对应的id
